2d360cb6ef4dffac546f7beba1ac6b9862e97bd9
Shankar Damodharan <shankardamodharan1998@gmail.com>
2024-07-13 17:08:53 +0530

    Issue fixes in accessing database models

--- test_proj1/settings.py
+++ test_proj1/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'test_proj1.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'practice',
         'USER': 'postgres',
         'PASSWORD': 'root',
